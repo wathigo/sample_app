e873481d4537e23fa71e6d569ea7f5b63378d2c7
wathigo <wathigosimon@gmail.com>
2019-07-03 18:20:16 -0400

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  root 'application#hello'
+  root 'static_pages#home'
 
   get  'static_pages/about'
+
+  get 'static_pages/contact'
 end
