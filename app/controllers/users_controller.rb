f1ec6f78aaf8f0fa2f3af56eed6073359d4d643e
wathigo <wathigosimon@gmail.com>
2019-07-22 05:47:18 -0400

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       log_in @user
+      remember @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,14 +4,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post users_path, params: { user: { name: "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
-    assert_select 'div.field_with_errors'
+    assert_select 'div.alert'
   end
 
   test "valid signup information" do
@@ -25,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
